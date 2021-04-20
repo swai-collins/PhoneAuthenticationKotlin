4d5b1b52aebd4bafd2de806cce9e25e5cfa7b601
Collins Swai <swailerc23@gmail.com>
2021-04-20 15:50:17 +0300

    Added food details

---
+++ app/src/main/java/com/example/phoneauthentication/Food.kt
@@ -0,0 +1,16 @@
+package com.example.phoneauthentication
+
+class Food {
+
+    var name : String? =  null
+    var description : String? = null
+    var image : Int? = null
+
+
+    constructor(name: String?, description :String?, image: Int? ){
+        this.name = name
+        this.description = description
+        this.image = image
+    }
+
+}
